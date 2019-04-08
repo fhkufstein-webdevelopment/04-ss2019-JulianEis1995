67371d5fa985a5e5b862c53a748d1fe0738ea471
Julian Eisenmann <julia@DESKTOP-5G1ORCP>
2019-04-08 19:14:55 +0200

    Aufgabe 2

--- js/user.js
+++ js/user.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
 
     var userListBody = $('.userList tbody');
 
-    //@todo store and somehow update the current number of users
-
-
     $('.needs-validation').submit(function(event) {
 
         event.preventDefault();
@@ -16,31 +13,43 @@ $(document).ready(function() {
 
             return false;
         }
+                    //Variablen definieren
+            var id = userListBody.find('tr').length;
+            var username = document.getElementById('username').value;
 
-        //@todo
-        //1. get values
-        //2. create a new element
-        //3. somehow add them to userListBody
-        //4. update number of current users
-        //5. clear entries from the form
-        //6. maybe do something else... :-)
+            // Tabelle erweitern
+            var table = document.getElementById('table');
+            var row = table.insertRow(-1);
 
-        //your code follows here
+            var cell0 = row.insertCell(0);
+            var cell1 = row.insertCell(1);
+            var cell2 = row.insertCell(2);
+
+            cell0.innerHTML = id;
+            cell1.innerHTML = username;
+            cell2.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"Löschen\"><i class=\"fa fa-trash\"></i></button>";
+
+            var counter = 0;
+            counter++;
+
+        $(".needs-validation")[0].reset();
 
         return false;
+
     });
 
 
     $('.deleteTrigger').click(function() {
-        //@todo
-        //1. remove current user from dom
-        //2. update number of current users
 
-        //your code follows here
+        //Info: bei dieser Möglichkeit wird lediglich die erste Zeile gelöscht, keine andere Zeile kann gelöscht werden.
+        //Überlegung war: Mithilfe von jQuery: "Closest" kann das am nächsten gelegene TR-Element gefunden und im Anschluss gelöscht werden. Praktischer Einzeiler.
 
+        if(!confirm("Löschen?")){
+            return;
+        }
+        $(this).closest("tr").remove();
+        counter--;
 
     });
-
-    //maybe some code follows here
 
 });
